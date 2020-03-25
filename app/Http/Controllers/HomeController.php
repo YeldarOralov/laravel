930172f45399fd9e7db0e7cfe02ecc9a3b5c3cdd
Eldar Oralov <eldar.oralov@gmail.com>
2020-03-26 00:45:30 +0600

    HomeController added. Book model added.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function index(Request $request){
+        return 'index';
+    }
+    function book(Book $book){
+        return $book;
+    }
+}
